60669d6f3e14d9fba28f9646b9888349f5590832
hfoliveira90 <143712286+hfoliveira90@users.noreply.github.com>
2024-06-28 20:10:53 -0300

    Exercícios Herança Java

---
+++ Exercícios/Herança/Pratica02/Animal.java
@@ -0,0 +1,31 @@
+package questao2;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Animal {
+    protected String nome;
+    protected double peso;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public Animal(String nome, double peso) {
+        this.nome = nome;
+        this.peso = peso;
+    }
+}
