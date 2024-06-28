2188578d4ac6763c3241e46b1ab416aec899dedb
hfoliveira90 <143712286+hfoliveira90@users.noreply.github.com>
2024-06-28 19:55:49 -0300

    Create Aluno.java

---
+++ Exercícios/CRUD/Pratica01/modelo/Aluno.java
@@ -0,0 +1,54 @@
+package modelo;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Aluno {
+    String cpf;
+    String nome;
+    String data;
+    float peso;
+    float altura;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+    
+}
